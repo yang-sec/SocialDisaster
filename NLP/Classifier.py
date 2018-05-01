50d5bbcf3513cb9dc42319b0490f452935830a7a
Sergio Sainz <sergio.sainz@gmail.com>
2018-04-30 22:47:50 -0400

    adding evaluation error

--- NLP/Classifier.py
+++ NLP/Classifier.py
@@ -23,23 +23,41 @@ class Classifier:
     def set_classifier(self, classifier):
         self.classifier = classifier
 
-    def prepare_test_train_set(self, pct, random_seed=13):
+    def prepare_test_train_set(self, pct, random_seed=13, pct_evaluation=0.8):
+
+        dataset = None
+        if not hasattr(self, 'eval_df'):
+            #First time, then, get the evaluation set:
+            # Choose which indices will belong to each set
+            num_articles = self.df.shape[0]
+            idx = range(num_articles)
+            self.df.index = idx
+            #print(idx)
+            num_train_test = int(num_articles * pct_evaluation)
+            num_evaluation = num_articles - num_train_test
+            train_test_idx = random.sample(idx, num_train_test)
+            eval_idx = [x for x in idx if x not in train_test_idx]
+
+            # Constructing the training and test sets
+            self.traintest_df = self.df.iloc[train_test_idx]
+            self.eval_df = self.df.iloc[eval_idx]
+
 
         if random_seed:
             random.seed(random_seed)
             
         # Choose which indices will belong to each set
-        num_articles = self.df.shape[0]
+        num_articles = self.traintest_df.shape[0]
         idx = range(num_articles)
-        self.df.index = idx
+        self.traintest_df.index = idx
         num_train = int(num_articles * pct)
         num_test = num_articles - num_train
         train_idx = random.sample(idx, num_train)
         test_idx = [x for x in idx if x not in train_idx]
 
         # Constructing the training and test sets
-        self.train_df = self.df.iloc[train_idx]
-        self.test_df = self.df.iloc[test_idx]
+        self.train_df = self.traintest_df.iloc[train_idx]
+        self.test_df = self.traintest_df.iloc[test_idx]
 
     def train_model(self):
         """ 
@@ -60,13 +78,13 @@ class Classifier:
         # Train the model
         self.classifier.fit(X,y)
 
-    def predict_labels(self):
+    def predict_labels(self, dataset):
         """
         Predicts the labels of the test set
         """
         # Turn the test data frame into an appropriate
         # matrix / vector
-        X = self.test_df.copy()
+        X = dataset.copy()
         del X['y']
         del X['eqID']
         del X['tweetID']
@@ -74,9 +92,9 @@ class Classifier:
         # Predict the label of the test examples
         self.ypred = self.classifier.predict(X)
         
-    def evaluate_results(self):
+    def evaluate_results(self, dataset):
         # Check the predictions against observed values
-        y = np.array(self.test_df['y'])
+        y = np.array(dataset['y'])
         y = y.astype(float)
         ypred = self.ypred
         # print(type(y))
@@ -87,17 +105,19 @@ class Classifier:
         return mean_squared_error
 
         
-    def bootstrap(self, iters=100, pct=0.8):
+    def bootstrap(self, iters=100, pct=0.8, pct_evaluation=0.8):
 
         results = []
         for i in range(iters):
             print("Iteration:["+str(i)+"]")
-            self.prepare_test_train_set(pct=pct)
+            self.prepare_test_train_set(pct=pct, pct_evaluation =pct_evaluation)
             self.train_model()
-            self.predict_labels()
-            stats = self.evaluate_results()
+            self.predict_labels(self.train_df)
+            stats = self.evaluate_results(self.train_df)
+            self.predict_labels(self.eval_df)
+            stats_eval = self.evaluate_results(self.eval_df)
             results.append(stats)
-            print("results iter:" + str(i) + " : " + str(stats))
+            print("results iter:" + str(i) + " : Test Error: " + str(stats) + " Evaluation error: " + str(stats_eval) )
         return results
         
 
@@ -111,13 +131,14 @@ class Classifier:
         """
         iters = 2
         pct = 0.75
+        pct_evaluation = 0.8
         
         for model in model_list:
             print(('Now testing %s') % (model['name']))
             self.set_classifier(model['classifier'])
             self.set_vectorizer(model['vectorizer_pickle_filename'])
             self.name = model['name']
-            results = self.bootstrap(iters=iters, pct=pct)
+            results = self.bootstrap(iters=iters, pct=pct, pct_evaluation=pct_evaluation)
             model['results'] = results
         
             print("Results for model %s " %(model['name']))
--- NLP/classifier_test.py
+++ NLP/classifier_test.py
@@ -1,8 +1,11 @@
 from Classifier import Classifier
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
+from sklearn.neural_network import MLPRegressor
 
 cls = RandomForestRegressor()
-
+cls_SVR = SVR()
+cls_MLPR = MLPRegressor(hidden_layer_sizes=(500, 250, 20))
 classifier = Classifier()
 models=[
     {
@@ -10,12 +13,38 @@ models=[
         # 'name':"randomforest_tfidf_world_2018",
         # 'vectorizer_pickle_filename': "tfidf_world_2018",
 
-        # 'name':"randomforest_tfidf_US_2008-2018",
-        # 'vectorizer_pickle_filename': "tfidf_US_2008-2018",
+        'name':"randomforest_tfidf_US_2008-2018",
+        'vectorizer_pickle_filename': "tfidf_US_2008-2018",
 
-        'name':"randomforest_tfidf_world_2008-2018",
-        'vectorizer_pickle_filename': "tfidf_world_2008-2018",
+        # 'name':"randomforest_tfidf_world_2008-2018",
+        # 'vectorizer_pickle_filename': "tfidf_world_2008-2018",
         
+        'classifier': cls_MLPR
+    },
+    {
+
+        # 'name':"randomforest_tfidf_world_2018",
+        # 'vectorizer_pickle_filename': "tfidf_world_2018",
+
+        'name': "randomforest_tfidf_US_2008-2018",
+        'vectorizer_pickle_filename': "tfidf_US_2008-2018",
+
+        # 'name':"randomforest_tfidf_world_2008-2018",
+        # 'vectorizer_pickle_filename': "tfidf_world_2008-2018",
+
+        'classifier': cls_SVR
+    },
+    {
+
+        # 'name':"randomforest_tfidf_world_2018",
+        # 'vectorizer_pickle_filename': "tfidf_world_2018",
+
+        'name': "randomforest_tfidf_US_2008-2018",
+        'vectorizer_pickle_filename': "tfidf_US_2008-2018",
+
+        # 'name':"randomforest_tfidf_world_2008-2018",
+        # 'vectorizer_pickle_filename': "tfidf_world_2008-2018",
+
         'classifier': cls
     }
 ]
